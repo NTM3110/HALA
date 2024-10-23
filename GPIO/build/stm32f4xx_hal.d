645e0a3016c89bb1e434f7e4dbe5a8fda6a5781e
NTM3110 <s3877477@rmit.edu.vn>
2024-10-23 20:09:17 +0700

    ADD GPIO

---
+++ GPIO/build/stm32f4xx_hal.d
@@ -0,0 +1,38 @@
+build/stm32f4xx_hal.o: Core/Src/stm32f4xx_hal.c Core/Inc/stm32f4xx_hal.h \
+ Core/Inc/stm32f4xx_hal_conf.h Core/Inc/stm32f4xx_hal_gpio.h \
+ Core/Inc/stm32f4xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ Drivers/CMSIS/Include/core_cm4.h Drivers/CMSIS/Include/cmsis_version.h \
+ Drivers/CMSIS/Include/cmsis_compiler.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Include/mpu_armv7.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ Core/Inc/Legacy/stm32_hal_legacy.h Core/Inc/stm32f4xx_hal_gpio_ex.h
+
+Core/Inc/stm32f4xx_hal.h:
+
+Core/Inc/stm32f4xx_hal_conf.h:
+
+Core/Inc/stm32f4xx_hal_gpio.h:
+
+Core/Inc/stm32f4xx_hal_def.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+Drivers/CMSIS/Include/core_cm4.h:
+
+Drivers/CMSIS/Include/cmsis_version.h:
+
+Drivers/CMSIS/Include/cmsis_compiler.h:
+
+Drivers/CMSIS/Include/cmsis_gcc.h:
+
+Drivers/CMSIS/Include/mpu_armv7.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+Core/Inc/Legacy/stm32_hal_legacy.h:
+
+Core/Inc/stm32f4xx_hal_gpio_ex.h:
